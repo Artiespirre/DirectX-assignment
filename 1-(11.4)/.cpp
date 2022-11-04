a8747b8e387bf16d06d044ff80e578b662ed89f1
Kim han jun <116341560+Artiespirre@users.noreply.github.com>
2022-11-04 17:33:26 +0900

    Update .cpp

--- 1-(11.4)/.cpp
+++ 1-(11.4)/.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+//Enter your code
 
+using namespace std;
+
+//Enter your code
+
+int main() {
+  
+//Enter your code
+	print << "TV의 소리는:" << //Enter your code << endl;
+	print << "TV의 채널은: " << //Enter your code << endl;
+}
